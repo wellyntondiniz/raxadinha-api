0489398311c616b3c5175b0e461cf2ccc3dccd3a
Wellynton Jose Diniz de Sousa <wellyntondiniz@gmail.com>
2026-05-04 19:44:49 -0400

    Projeto inicial

---
+++ src/main/java/com/raxadinha/RaxadinhaApiApplication.java
@@ -0,0 +1,13 @@
+package com.raxadinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaxadinhaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaxadinhaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raxadinha/RaxadinhaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raxadinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaxadinhaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
