25f3dcd1d2c12cf223af6b2f82298f9dadb30a18
Lucas Brembati Weizenmann <lucas8bitbw@gmail.com>
2026-05-14 20:52:11 -0400

    add data init e fim

--- src/main/java/com/raxadinha/evento/Evento.java
+++ src/main/java/com/raxadinha/evento/Evento.java
@@ -1,5 +1,7 @@
 package com.raxadinha.evento;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,6 +17,9 @@ public class Evento {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@Column(name = "nome")
+	private String nome;
+	
 	@Column(name = "grupo_id")
 	private Integer grupoId;
 	
@@ -24,12 +29,26 @@ public class Evento {
 	@Column(name = "descricao")
 	private String descricao;
 	
+	@Column(name = "data_inicio")
+	private LocalDate data_inicio;
+	
+	@Column(name = "data_termino")
+	private LocalDate data_termino;
+	
 	public Integer getId() {
 		return id;
 	}
 	
 	public void setId(Integer id) {
 		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 	
 	public Integer getGrupoId() {
@@ -54,5 +73,21 @@ public class Evento {
 	
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+	
+	public LocalDate getDataInicio() {
+		return data_inicio;
+	}
+	
+	public void setDataInicio(LocalDate data_inicio) {
+		this.data_inicio = data_inicio;
+	}
+	
+	public LocalDate getDataTermino() {
+		return data_termino;
+	}
+	
+	public void setDataTermino(LocalDate data_termino) {
+		this.data_termino = data_termino;
 	}
 }
