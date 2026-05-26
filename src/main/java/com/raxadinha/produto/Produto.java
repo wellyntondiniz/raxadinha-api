3668ed25132a86517abf14e55cc314c2b026f148
Gabriel Schirrmann Faria <gabrielschirrmannfarias@gmail.com>
2026-05-25 20:59:59 -0400

    Cadastro de produtos

--- src/main/java/com/raxadinha/produto/Produto.java
+++ src/main/java/com/raxadinha/produto/Produto.java
@@ -1,5 +1,24 @@
 package com.raxadinha.produto;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "produto")
+@Data
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "descricao", nullable = false)
+    private String descricao;
+
+    @Column(name = "unidade_de_medida", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UnidadeDeMedida unidadeDeMedida;
+
+    @Column(name = "quantidade", nullable = false)
+    private Double quantidade;
 }
