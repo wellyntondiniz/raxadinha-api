fe6c42a18c792cbf31b7f013ce16549013412f4d
Wellynton Jose Diniz de Sousa <wellyntondiniz@gmail.com>
2026-06-01 19:20:16 -0400

    getter e setter produto

--- src/main/java/com/raxadinha/produto/Produto.java
+++ src/main/java/com/raxadinha/produto/Produto.java
@@ -21,4 +21,38 @@ public class Produto {
 
     @Column(name = "quantidade", nullable = false)
     private Double quantidade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public UnidadeDeMedida getUnidadeDeMedida() {
+		return unidadeDeMedida;
+	}
+
+	public void setUnidadeDeMedida(UnidadeDeMedida unidadeDeMedida) {
+		this.unidadeDeMedida = unidadeDeMedida;
+	}
+
+	public Double getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Double quantidade) {
+		this.quantidade = quantidade;
+	}
+    
+    
 }
